2a87e9a91a80b562df1b8b7af263dcf3bd247ea3
Larry Kagan <larry@superiocity.com>
2022-06-10 15:38:44 -0400

    Two sum brute force in PHP

---
+++ two-sum/brute-force.php
@@ -0,0 +1,15 @@
+<?php
+function findTwoSum($nums, $target) 
+{
+    for ($i = 0; $i < count($nums) - 1; ++$i) {
+        for ($j = 1; $j < count($nums); ++$j) {
+            if ($nums[$i] + $nums[$j] === $target) {
+                return [$i, $j];
+            }
+        }
+    }
+}
+
+print(json_encode(findTwoSum([ 2, 7, 11, 15 ], 9)) . PHP_EOL); // [0, 1]
+print(json_encode(findTwoSum([ 3, 2, 4 ], 6)) . PHP_EOL); // [1, 2]
+print(json_encode(findTwoSum([ 3, 3 ], 6)) . PHP_EOL); // [0, 1]
