b468ab6eee9836ef13a3d34246b7558d31aec9d8
Larry Kagan <larry@superiocity.com>
2022-06-29 13:56:13 -0400

    Add water-area brute force python solution

---
+++ water-area/brute-force.py
@@ -0,0 +1,13 @@
+def greatest_volume_pair(nums):
+    largest_area = 0
+
+    for p1 in range(len(nums) - 1):
+        for p2 in range(len(nums)):
+            length = p2 - p1
+            width = min(nums[p1], nums[p2])
+            largest_area = max(largest_area, length * width)
+
+    return largest_area
+
+print(greatest_volume_pair([ 7, 1, 3, 9 ]))
+
