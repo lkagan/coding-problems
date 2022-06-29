bbfc3d4a247d45ac3f3b567b126236be350efc5b
Larry Kagan <larry@superiocity.com>
2022-06-29 14:29:38 -0400

    Added print utility function for PHP

--- two-sum/hash-map.php
+++ two-sum/hash-map.php
@@ -1,4 +1,5 @@
 <?php
+require '../utils/utils.php';
 
 function twoSum (array $nums, int $target)
 {
@@ -18,10 +19,8 @@ function twoSum (array $nums, int $target)
 
 }
 
-$pj = fn($val) => (print(json_encode($val)) . PHP_EOL);
-
-$pj(twoSum([ 2, 7, 11, 15 ], 9)); // [0, 1]
-$pj(twoSum([ 3, 2, 4 ], 6)); // [1, 2]
-$pj(twoSum([ 3, 3 ], 6)); // [0, 1]
-$pj(twoSum([3], 6)); // null
-$pj(twoSum([], 6)); // null
+printJson(twoSum([ 2, 7, 11, 15 ], 9)); // [0, 1]
+printJson(twoSum([ 3, 2, 4 ], 6)); // [1, 2]
+printJson(twoSum([ 3, 3 ], 6)); // [0, 1]
+printJson(twoSum([3], 6)); // null
+printJson(twoSum([], 6)); // null
---
+++ utils/utils.php
@@ -0,0 +1,6 @@
+<?php
+
+function printJson($val)
+{
+    print(json_encode($val)) . PHP_EOL;
+}
