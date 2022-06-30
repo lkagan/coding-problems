e246a36ab3752e88d7e888a0827397f64a6b9fb2
Larry Kagan <larry@superiocity.com>
2022-06-30 18:12:11 -0400

    Add type hints

--- 02-most-water/two-pointer.py
+++ 02-most-water/two-pointer.py
@@ -1,11 +1,11 @@
-def find_max_area(nums):
-    max_area = 0
-    p1 = 0
-    p2 = len(nums) - 1
-    width = p2 - p1
+def find_max_area(nums: list) -> int:
+    max_area: int = 0
+    p1: int = 0
+    p2: int = len(nums) - 1
+    width: int = p2 - p1
 
     while width > 0:
-        area = width * min(nums[p1], nums[p2])
+        area: int = width * min(nums[p1], nums[p2])
         max_area = max(max_area, area)
 
         if nums[p1] < nums[p2]:
@@ -18,6 +18,6 @@ def find_max_area(nums):
     return max_area
 
 
-print(find_max_area([5,3,2,8,7]))
+print(find_max_area([5, 3, 2, 8, 7]))
 print(find_max_area([5]))
 print(find_max_area([]))
