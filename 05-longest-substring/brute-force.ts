3fa6aba7aa539c307ea00e606402a6649a13ec21
Larry Kagan <larry@superiocity.com>
2022-07-26 12:11:41 -0400

    Longest substring brute force in PHP

--- 05-longest-substring/brute-force.ts
+++ 05-longest-substring/brute-force.ts
@@ -2,7 +2,6 @@ var lengthOfLongestSubstring = function(s:string): number {
     let longest = 0;
 
     for (let i = 0; i < s.length; ++i) {
-        const letterTocompare:string = s[i];
         let currentLength = 0;
         const seen = {};
 
