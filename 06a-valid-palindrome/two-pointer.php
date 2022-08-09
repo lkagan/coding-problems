eea02d8cbbc685ee690806d9eabd2294f1cb3151
Larry Kagan <larry@superiocity.com>
2022-08-09 16:28:11 -0400

    Is a palindrome two-pointer solution in PHP

---
+++ 06a-valid-palindrome/two-pointer.php
@@ -0,0 +1,21 @@
+<?php
+function isPalindrome($s)
+{
+    $s     = strtolower(preg_replace('/[^a-zA-Z0-9]/', '', $s));
+    $leftP  = 0;
+    $rightP = strlen($s) - 1;
+
+    while ($leftP < $rightP) {
+        if ($s[$leftP] !== $s[$rightP]) {
+            return false;
+        }
+
+        $leftP++;
+        $rightP--;
+    }
+
+    return true;
+}
+
+isPalindrome("race a car");
+
