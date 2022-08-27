dcfa37e8d2ccd058e882a92dc377ddb5dd3d4f1c
Larry Kagan <larry@superiocity.com>
2022-08-27 10:49:40 -0400

    Valid parenthesis in TypeScript

---
+++ 10-valid-parenthesis/20220827.ts
@@ -0,0 +1,22 @@
+function isValid(s: string): boolean {
+    const pairs = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    const left = new Set(Object.values(pairs));
+    const unmatched: string[] = [];
+
+    for (let i = 0; i < s.length; ++i) {
+        const c = s[i];
+
+        if (left.has(c)) {
+            unmatched.push(c);
+        } else if (unmatched.pop() !== pairs[c]) {
+            return false;
+        }
+    }
+
+    return unmatched.length === 0;
+}
