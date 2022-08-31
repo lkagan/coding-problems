b7fd5a87bf1dc732aba040137b440dcad53d2738
Larry Kagan <larry@superiocity.com>
2022-08-31 12:56:04 -0400

    Bubble sort in Javascript

---
+++ miscellaneous/bubble-sort.js
@@ -0,0 +1,18 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(numbers) {
+    for (let i = 0; i < numbers.length; ++i) {
+        for (let j = 0; j < numbers.length - 1; j++) {
+            if (numbers[j] > numbers[j+1]) {
+                let temp = numbers[j];
+                numbers[j] = numbers[j+1];
+                numbers[j+1] = temp;
+            }
+        }
+    }
+
+    return numbers;
+}
+
+bubbleSort(numbers);
+console.log(numbers);
