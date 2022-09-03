530c11e4f9e43eb2217be999077973856b62f274
Larry Kagan <larry@superiocity.com>
2022-09-03 14:16:45 -0400

    Selection sort in TypeScript

--- miscellaneous/selection-sort.js
+++ miscellaneous/selection-sort.js
@@ -18,20 +18,15 @@ function selectionSort(nums) {
     return nums;
 }
 
-function selectionSort(nums) {
+// Slightly more overhead but easier to follow.
+function selectionSortSimplified(nums) {
     for (let i = 0; i < nums.length - 1; ++i) {
-        let lowestIdx = i;
-
         for (let j = i + 1; j < nums.length; ++j) {
-            if (nums[j] < nums[lowestIdx]) {
-                lowestIdx = j;
+            if (nums[j] < nums[i]) {
+                let temp = nums [i];
+                nums[i] = nums[j];
+                nums[j] = temp;
             }
-        }
-
-        if (lowestIdx !== i) {
-            let lowest = nums [lowestIdx];
-            nums[lowestIdx] = nums[i];
-            nums[i] = lowest;
         }
     }
 
@@ -39,5 +34,5 @@ function selectionSort(nums) {
 }
 
 const numbers = [3,8,1,3,3,5,7,1,2,6,2,7,0,1];
-// const numbers = [0,3,1,2];
 console.log(selectionSort(numbers));
+console.log(selectionSortSimplified(numbers));
