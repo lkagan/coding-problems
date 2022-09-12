dbe981217ddba00c9c73cef38644cf86d8d0ed0b
Larry Kagan <larry@superiocity.com>
2022-09-12 18:53:59 -0400

    Kith largest element in TypeScript

---
+++ 13-kth-largest-element/20220910.ts
@@ -0,0 +1,46 @@
+function findKthLargest(nums: number[], k: number): number {
+    const indexToFind = nums.length - k;
+    return quickSelect(nums, 0, nums.length - 1, indexToFind);
+}
+
+function quickSelect(
+    nums: number[],
+    start: number,
+    end: number,
+    indexToFind: number
+): number {
+    if (start === end) {
+        return nums[start];
+    }
+
+    let left: number = start;
+    let right: number = start;
+
+    while (right < end) {
+        if (nums[right] <= nums[end]) {
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+            left++;
+        }
+
+        right++;
+    }
+
+    [nums[left], nums[end]] = [nums[end], nums[left]];
+
+    if (left === indexToFind) {
+        return nums[indexToFind];
+    } else if (left > indexToFind) {
+        return quickSelect(nums, start, left - 1, indexToFind);
+    } else {
+        return quickSelect(nums, left + 1, end, indexToFind);
+    }
+}
+
+
+let nums, k;
+nums = [3, 2, 1, 5, 6, 4];
+k = 2 // 5
+
+nums = [3,2,3,1,2,4,5,5,6]
+k = 4;
+console.log(findKthLargest(nums, k));
