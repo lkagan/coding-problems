b5bf962b0710a32eb27bac9c1a40f6249f83bb14
Larry Kagan <larry@superiocity.com>
2022-09-13 13:19:29 -0400

    Binary search in TypeScript

---
+++ miscellaneous/search/binary.ts
@@ -0,0 +1,23 @@
+function binarySearch(nums: number[], term: number): number | null {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (term === nums[mid]) {
+            return mid;
+        } else if (term < nums[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return null;
+}
+
+let nums = [1,2,3,4,5,7];
+let term = 6;
+
+console.log(binarySearch(nums, term));
