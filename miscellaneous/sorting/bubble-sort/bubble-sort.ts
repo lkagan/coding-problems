aebf1e2ac7800768923f3aea9c05b7920d01bff9
Larry Kagan <larry@superiocity.com>
2022-09-17 15:08:14 -0400

    Optimize bubble sort in TypeScript

--- miscellaneous/sorting/bubble-sort/bubble-sort.ts
+++ miscellaneous/sorting/bubble-sort/bubble-sort.ts
@@ -1,17 +1,32 @@
-function bubbleSort(numbers: number[]): number[] {
-    for (let i = 0; i < numbers.length; ++i) {
-        for (let j = 0; j < numbers.length - 1; j++) {
-            if (numbers[j] > numbers[j+1]) {
-                let temp: number = numbers[j];
-                numbers[j] = numbers[j+1];
-                numbers[j+1] = temp;
+function bubbleSort(nums: number[]): number[] {
+    for (let i = 0; i < nums.length; ++i) {
+        // Assume sorted. No need to iterate if already sorted.
+        let sorted = true;
+
+        for (let j = 0; j < nums.length - i; ++j) {
+            if (nums[j] > nums[j + 1]) {
+                [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+                sorted = false;  // Discovered array isn't sorted.
             }
+        }
+
+        if (sorted) {
+            return nums;
         }
     }
 
-    return numbers;
+    return nums;
 }
 
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
-bubbleSort(numbers);
-console.log(numbers);
+const numbers = [
+    [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0],
+    [1, 2, 3, 4, 5, 6, 9, 100],
+    [99, 44, 6, 2],
+    [2, 1],
+    [2],
+    []
+];
+
+for (const nums of numbers) {
+    console.log(bubbleSort(nums));
+}
