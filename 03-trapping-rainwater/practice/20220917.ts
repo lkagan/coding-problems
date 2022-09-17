45ddbae1234a8b7128d34e5d2f528944f9b0033c
Larry Kagan <larry@superiocity.com>
2022-09-17 18:03:08 -0400

    Practice trapping rainwater in TypeScript

---
+++ 03-trapping-rainwater/practice/20220917.ts
@@ -0,0 +1,29 @@
+funciton trap (heights: number[]): number {
+    let maxLeft = 0;
+    let maxRight = 0;
+    let left = 0;
+    let right = heights.length - 1;
+    let total = 0;
+
+    while (left < right) {
+        if (heights[left] <= heights[right]) {
+           if (heights[left] >= maxLeft) {
+               maxLeft = heights[left];
+           }  else {
+               total += (maxLeft - heights[left]);
+           }
+
+           ++left;
+        } else {
+            if (heights[right] >= maxRight) {
+                maxRight = heights[right];
+            } else {
+                total += (maxRight - heights[right]);
+            }
+
+            --right;
+        }
+    }
+
+    return total;
+}
