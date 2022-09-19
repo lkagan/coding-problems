42a1facfa73bc9c8291313571c8bbf9fd41ea285
Larry Kagan <larry@superiocity.com>
2022-09-19 18:43:03 -0400

    Backspace string compare using 2 pointers in Javasscript

---
+++ 04-backspace-string-compare/practice/20220919-2pointers.js
@@ -0,0 +1,44 @@
+function backspaceCompare(s, t) {
+    let sHashes = 0, tHashes = 0;
+    let sPointer = s.length - 1;
+    let tPointer = t.length - 1;
+
+    while (sPointer >= 0 || tPointer >= 0) {
+        let sChar = null, tChar = null;
+
+        // get next s char
+        while (sPointer >= 0) {
+           if (s[sPointer] === '#') {
+               sHashes++;
+               sPointer--;
+           } else if (sHashes) {
+               sHashes--;
+               sPointer--;
+           } else {
+               sChar = s[sPointer];
+               sPointer--;
+               break;
+           }
+        }
+
+        while (tPointer >= 0) {
+            if (t[tPointer] === '#') {
+                tHashes++;
+                tPointer--;
+            } else if (tHashes) {
+                tHashes--;
+                tPointer--;
+            } else {
+                tChar = t[tPointer];
+                tPointer--;
+                break;
+            }
+        }
+
+        if (tChar !== sChar) {
+            return false;
+        }
+    }
+
+    return true;
+}
