c88fa745b5717bea83d8ad09b6e008a34365dbed
Larry Kagan <larry@superiocity.com>
2022-10-11 15:49:32 -0400

    Bubble sort in TypeScript

---
+++ miscellaneous/sorting/bubble-sort/20221011/bubble-sort-ts.ts
@@ -0,0 +1,13 @@
+export default (nums: number[]): number[] => {
+    if (nums.length < 2) {
+        return nums;
+    }
+
+    for (let i = nums.length - 2; i >= 0; --i) {
+        for (let j = 0; j <= i; ++j) {
+            nums[j] > nums[j+1] && ([nums[j], nums[j+1]] = [nums[j+1], nums[j]]);
+        }
+    }
+
+    return nums;
+}
---
+++ miscellaneous/sorting/bubble-sort/20221011/bubble-sort.test.ts
@@ -0,0 +1,17 @@
+import bubbleSort from './bubble-sort-ts';
+
+const inputs = [
+    [],
+    [1],
+    [2, 1],
+    [4, 1, 3, 2, 5]
+];
+
+describe('Bubble sort', () => {
+    inputs.forEach(nums => {
+
+        it('Sorts ' + JSON.stringify(nums), () => {
+            expect(bubbleSort(nums)).toEqual([...nums].sort((a,b) => a - b));
+        });
+    });
+});
