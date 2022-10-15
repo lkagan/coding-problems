bf3be048c02101a608aa839fafb7a46a6b5a019c
Larry Kagan <larry@superiocity.com>
2022-10-15 13:45:36 -0400

    Insertion sort and test in TypeScript

---
+++ miscellaneous/sorting/insertion-sort/20221014/insertion-sort.test.ts
@@ -0,0 +1,16 @@
+import insertionSort from './insertion-sort-ts';
+
+const testCases = [
+    [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0],
+    [1, 2, 3, 4, 5, 6, 9, 100],
+    [99, 44, 6, 2],
+    [2, 1],
+    [2],
+    []
+];
+
+testCases.forEach(nums => {
+    test(`insertionSort([${nums}])`, () => {
+        expect(insertionSort([...nums])).toEqual([...nums].sort((a,b) => a - b));
+    })
+})
