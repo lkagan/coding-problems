d85d469e014b96fbb744b619732fb18e16295547
Larry Kagan <larry@superiocity.com>
2022-10-18 17:38:36 -0400

    Right side tree view and test in Javascript

---
+++ 17-right-side-tree/20221018/right-side-view.js
@@ -0,0 +1,21 @@
+'strict mode';
+
+module.exports = (root) => {
+    const queue = [root];
+    const results = [];
+
+    while (queue.length) {
+        let levelLength = queue.length;
+        let node;
+
+        while (levelLength--) {
+            node = queue.shift();
+            node.left && queue.push(node.left);
+            node.right && queue.push(node.right);
+        }
+
+        results.push(node.val);
+    }
+
+    return results;
+}
---
+++ 17-right-side-tree/20221018/right-side-view.test.js
@@ -0,0 +1,21 @@
+'strict mode';
+
+const rightSideView = require('./right-side-view');
+
+class TreeNode {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+const tree = new TreeNode(1);
+tree.left = new TreeNode(2);
+tree.left.right = new TreeNode(5);
+tree.right = new TreeNode(3);
+tree.right.right = new TreeNode(4);
+
+test('rightSideView', () => {
+    expect(rightSideView(tree)).toEqual([1,3,4]);
+});
