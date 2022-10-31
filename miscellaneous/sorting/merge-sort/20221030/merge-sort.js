0ee85512209a80dff239661c725d2fac1a8d34b0
Larry Kagan <larry@superiocity.com>
2022-10-30 20:28:49 -0400

    Merge sort and test in Javascript

---
+++ miscellaneous/sorting/merge-sort/20221030/merge-sort.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function mergeSort(nums) {
+    if (nums.length <= 1) {
+        return nums;
+    }
+
+    const mid = Math.floor(nums.length / 2);
+    return merge(
+        mergeSort(nums.slice(0, mid)),
+        mergeSort(nums.slice(mid))
+    );
+}
+
+function merge(left, right) {
+    const results = [];
+
+    while(left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+
+    return [...results, ...left, ...right];
+}
+
+module.exports = mergeSort;
