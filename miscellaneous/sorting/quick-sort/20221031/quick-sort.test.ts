946de1e0c963a4deac99962a84def960816535f5
Larry Kagan <larry@superiocity.com>
2022-11-03 21:21:18 -0400

    Quicksort with test in TypeScript

---
+++ miscellaneous/sorting/quick-sort/20221031/quick-sort.test.ts
@@ -0,0 +1,17 @@
+import quickSort from './quick-sort-ts';
+
+const testCases = [
+    [],
+    [2],
+    [2, 1],
+    [99, 44, 6, 2],
+    [1, 2, 3, 4, 5, 6, 9, 100],
+    [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0],
+];
+
+testCases.forEach(nums => {
+    it(`quickSort([${nums}])`, () => {
+        expect(quickSort([...nums], 0, nums.length - 1))
+            .toEqual([...nums].sort((a,b) => a - b));
+    });
+});
