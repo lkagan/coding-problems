92bebd1b7e9a33736ec6cfca7b30f93b594ddf02
Larry Kagan <larry@superiocity.com>
2022-11-06 21:05:10 -0500

    Bubblesort and test in TypeScript

---
+++ miscellaneous/sorting/bubble-sort/20221106/bubble-sort-ts.ts
@@ -0,0 +1,18 @@
+export default (nums: number[]): number[] => {
+    let sorted = true;
+
+    for (let i = 0; i < nums.length; ++i) {
+        for (let j = 0; j < nums.length - i - 1; ++j) {
+            if (nums[j] > nums[j+1]) {
+                [nums[j], nums[j+1]] = [nums[j+1], nums[j]];
+                sorted = false;
+            }
+        }
+
+        if (sorted) {
+            return nums;
+        }
+    }
+
+    return nums;
+}
