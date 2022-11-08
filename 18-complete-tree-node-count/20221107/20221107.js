18bdafb48356c928f942a0b3ae9a4a61ca2aff9e
Larry Kagan <larry@superiocity.com>
2022-11-07 20:57:28 -0500

    WIP: complete but not working in JS

--- 18-complete-tree-node-count/20221107/20221107.js
+++ 18-complete-tree-node-count/20221107/20221107.js
@@ -1,16 +1,27 @@
 'use strict';
 
 function countNodes(root) {
-    if (root.next === null) {
+    if (!root) {
+        return 0;
+    }
+
+    if (!root.next) {
         return 1;
     }
 
     const height = getHeight(root);
-    // Get height
-    // Get number of nodes in all but last level
-    // Get number of nodes in last level
-    // Add last level to all levels above
-    // Return
+    const topNodesCount = 2 ** (height - 1) - 1;
+
+    // Find the index of the last node
+    let left = 0;
+    let right = topNodesCount + 1;
+
+    while (left < right) {
+        let mid = Math.ceil((right + left) / 2);
+        nodeExists(mid, height, root) ? left = mid : right = mid - 1;
+    }
+
+    return topNodesCount + left + 1;
 }
 
 function getHeight(root) {
@@ -22,6 +33,28 @@ function getHeight(root) {
     }
 
     return count;
+}
+
+function nodeExists(idxToFind, height, node) {
+    let left = 0;
+    let right = 2 ** (height - 1) - 1;
+    let count = 0;
+
+    while (count < height) {
+        let mid = Math.ceil((left + right) / 2);
+        
+        if (idxToFind >= mid) {
+            node = node.right;
+            left = mid;
+        } else {
+            node = node.left;
+            right = mid - 1;
+        }
+
+        count++;
+    }
+
+    return node !== null;
 }
 
 class TreeNode {
