3d934cd2363f92c2d0b4d5526c6d255156aa96b3
Larry Kagan <larry@superiocity.com>
2022-11-27 15:24:15 -0500

    Refactor BFS traversal of matrix to a class

--- miscellaneous/matrix-bfs-traversal/20221123/20221126-ts.ts
+++ miscellaneous/matrix-bfs-traversal/20221123/20221126-ts.ts
@@ -1,42 +1,44 @@
-export default (matrix: number[][]): number[] => {
-    const results: number[] = [];
-    const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+export default class Matrix {
+    private seen;
 
-    const seen = [...Array(matrix.length)].map(() => {
-        return new Array(matrix[0].length).fill(false);
-    });
-
-    const queue = [[0, 0]];
-    seen[0][0] = true;
-
-    while (queue.length) {
-        let [row, col] = queue.shift()!;
-        results.push(matrix[row][col]);
-
-        dirs.forEach(dir => {
-            const nextRow = row + dir[0];
-            const nextCol = col + dir[1];
-
-            if (validCell(nextRow, nextCol, matrix, seen)) {
-                seen[nextRow][nextCol] = true;
-                queue.push([nextRow, nextCol]);
-            }
+    constructor(private matrix: number[][]) {
+        this.seen = [...Array(matrix.length)].map(() => {
+            return new Array(matrix[0].length).fill(false);
         });
     }
 
-    return results;
-}
+    public flatten(): number[] {
+        const results: number[] = [];
+        const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
-function validCell(
-    row: number,
-    col: number,
-    matrix: number[][],
-    seen: boolean[][]
-): boolean {
-    if (row < 0 || col < 0 || row >= matrix.length
-        || col >= matrix[0].length || seen[row][col]) {
-        return false;
+
+        const queue = [[0, 0]];
+        this.seen[0][0] = true;
+
+        while (queue.length) {
+            let [row, col] = queue.shift()!;
+            results.push(this.matrix[row][col]);
+
+            dirs.forEach(dir => {
+                const nextRow = row + dir[0];
+                const nextCol = col + dir[1];
+
+                if (this.validCell(nextRow, nextCol)) {
+                    this.seen[nextRow][nextCol] = true;
+                    queue.push([nextRow, nextCol]);
+                }
+            });
+        }
+
+        return results;
     }
 
-    return true;
+    protected validCell(row: number, col: number): boolean {
+        if (row < 0 || col < 0 || row >= this.matrix.length
+            || col >= this.matrix[0].length || this.seen[row][col]) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- miscellaneous/matrix-bfs-traversal/20221123/20221126.test.ts
+++ miscellaneous/matrix-bfs-traversal/20221123/20221126.test.ts
@@ -1,14 +1,15 @@
-import matrixBfs from './20221126-ts';
-
-const matrix = [
+import Matrix from './20221126-ts';
+const matrixTest = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 10, 11, 12],
     [13, 14, 15, 16]
 ];
 
+const matrix = new Matrix(matrixTest);
+
 const results = [1, 2, 5, 3, 6, 9, 4, 7, 10, 13, 8, 11, 14, 12, 15, 16];
 
 it('matrixBfs(matrix)', () => {
-    expect(matrixBfs(matrix)).toEqual(results);
+    expect(matrix.flatten()).toEqual(results);
 })
