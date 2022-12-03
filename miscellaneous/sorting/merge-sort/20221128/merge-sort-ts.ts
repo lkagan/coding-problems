0b83da074bae4d0da9af4ec6550975e1ffd7c3eb
Larry Kagan <larry@superiocity.com>
2022-12-03 07:35:33 -0500

    Merge sort and test in TypeScript

---
+++ miscellaneous/sorting/merge-sort/20221128/merge-sort-ts.ts
@@ -0,0 +1,26 @@
+function mergeSort(nums: number[]): number[] {
+    if (nums.length <= 1) return nums;
+
+    const mid = Math.floor(nums.length / 2);
+
+    return merge(
+        mergeSort(nums.slice(0, mid)),
+        mergeSort(nums.slice(mid))
+    );
+}
+
+function merge(left: number[], right: number[]): number[] {
+    const results: number[] = [];
+
+    while (left.length && right.length) {
+        if (left[0] > right[0]) {
+            results.push(right.shift()!);
+        } else {
+            results.push(left.shift()!);
+        }
+    }
+
+    return [...results, ...left, ...right];
+}
+
+export default mergeSort;
