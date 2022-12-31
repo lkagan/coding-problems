df87b8d30e0523ec306746bb0c73aea734c64c50
Larry Kagan <larry@superiocity.com>
2022-12-30 21:52:04 -0500

    Insertion sort with test in Python

---
+++ miscellaneous/sorting/insertion-sort/20221120/insertion_sort_test.py
@@ -0,0 +1,14 @@
+from insertion_sort import insertion_sort
+
+testCases = [
+    [],
+    [2],
+    [2, 1],
+    [99, 44, 6, 2],
+    [1, 2, 3, 4, 5, 6, 9, 100],
+    [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0],
+]
+
+def test_insertion_sort() -> None:
+    for nums in testCases:
+        assert insertion_sort(nums) == sorted(nums)
