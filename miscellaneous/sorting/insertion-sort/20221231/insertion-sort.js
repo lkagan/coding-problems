f470d31288efcd12b312e8493b26282d417e68a1
Larry Kagan <larry@superiocity.com>
2022-12-31 21:52:14 -0500

    Insertion sort and test in Javascript

---
+++ miscellaneous/sorting/insertion-sort/20221231/insertion-sort.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.insertionSort = (nums) => {
+    for (let i = 1; i < nums.length; ++i) {
+        let j = i - 1;
+        const current = nums[i];
+
+        while (j >= 0 && current < nums[j]) {
+            nums[j+1] = nums[j--];
+        }
+
+        nums[++j] = current
+    }
+
+    return nums;
+}
+
