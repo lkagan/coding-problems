1ddb09948d742c96253e799d5668480b94d79105
Larry Kagan <larry@superiocity.com>
2023-01-06 08:54:34 -0500

    Merge sort and test in PHP

---
+++ miscellaneous/sorting/merge-sort/20230103/MergeSortTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+require __DIR__ . '/merge-sort.php';
+use PHPUnit\Framework\TestCase;
+
+class MergeSortTest extends TestCase
+{
+    private array $testCases;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->testCases = [
+            [],
+            [2],
+            [2, 1],
+            [99, 44, 6, 2],
+            [1, 2, 3, 4, 5, 6, 9, 100],
+            [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0],
+        ];
+
+    }
+    public function test_merge_sort()
+    {
+        foreach ($this->testCases as $nums) {
+            $sorted = $nums;
+            sort($sorted);
+            $this->assertEquals(expected: $sorted, actual: mergeSort($nums));
+        }
+    }
+}
---
+++ miscellaneous/sorting/merge-sort/20230103/merge-sort.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+function mergeSort(array $nums): array
+{
+    if (count($nums) <= 1) {
+        return $nums;
+    }
+
+    $mid = (int) floor(count($nums) / 2);
+
+    return merge(
+        mergeSort(array_slice($nums, 0, $mid)),
+        mergeSort(array_slice($nums, $mid))
+    );
+}
+
+function merge(array $left, array $right): array
+{
+    $merged = [];
+
+    while (count($left) && count($right)) {
+        $merged[] = $left[0] < $right[0] ? array_shift($left) : array_shift($right);
+    }
+
+    return [...$merged, ...$left, ...$right];
+}
