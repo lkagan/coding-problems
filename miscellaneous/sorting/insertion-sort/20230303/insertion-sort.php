1cbe84f2dd6c6839817f3be89a77862fb6a14a1c
Larry Kagan <larry@superiocity.com>
2023-03-05 09:48:35 -0500

    Insertion sort with test in PHP

---
+++ miscellaneous/sorting/insertion-sort/20230303/insertion-sort.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+function insertionSort(array $nums): array
+{
+    if (count($nums) <= 1) return $nums;
+
+    for ($i = 1; $i < count($nums); ++$i) {
+        $j = $i - 1;
+        $current = $nums[$i];
+
+        while ($j >= 0 && $nums[$j] > $current) {
+            $nums[$j+1] = $nums[$j--];
+        }
+
+        $nums[++$j] = $current;
+    }
+
+    return $nums;
+}
