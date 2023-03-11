ed4b7526afbb6dc35d77ac7295dd9b672e8dca7b
Larry Kagan <larry@superiocity.com>
2023-03-11 16:12:30 -0500

    Matrix BFS traversal with test in Python

---
+++ miscellaneous/matrix-bfs-traversal/20230307/matrix_bfs.py
@@ -0,0 +1,28 @@
+from typing import Tuple, List
+
+DIRS = (-1, 0), (0, 1), (1, 0), (0, -1)
+
+
+def bfs(matrix: Tuple[Tuple[int]]) -> Tuple[int]:
+    values: List[int] = []
+    seen = {matrix[0][0]}
+    queue = [(0, 0)]
+
+    while queue:
+        row, col = queue.pop(0)
+        values.append(matrix[row][col])
+
+        for yOffset, xOffset in DIRS:
+            new_row = row + yOffset
+            new_col = col + xOffset
+
+            if (in_bounds(new_row, new_col, matrix) and
+                    matrix[new_row][new_col] not in seen):
+                queue.append((new_row, new_col))
+                seen.add(matrix[new_row][new_col])
+
+    return tuple(values)
+
+
+def in_bounds(row: int, col: int, matrix: Tuple[Tuple[int]]) -> bool:
+    return 0 <= row < len(matrix) and 0 <= col < len(matrix[0]);
