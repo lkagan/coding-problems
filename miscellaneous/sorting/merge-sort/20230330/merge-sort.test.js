84ceab71e01bfacdc02a7fdb7b3bf2f7f787315b
Larry Kagan <larry@superiocity.com>
2023-04-01 23:40:12 -0400

    Merge sort with test in Javascript.

---
+++ miscellaneous/sorting/merge-sort/20230330/merge-sort.test.js
@@ -0,0 +1,18 @@
+const sort = require('./merge-sort');
+
+const testData = [
+    [],
+    [2],
+    [2, 1],
+    [99, 44, 6, 2],
+    [1, 2, 3, 4, 5, 6, 9, 100],
+    [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0],
+];
+
+describe('Sorts using merge sort', () => {
+    testData.forEach(nums => {
+        it(`sorts [${nums}]`, () => {
+            expect(sort([...nums])).toEqual([...nums].sort((a,b) => a - b));
+        })
+    })
+})
