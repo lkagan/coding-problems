7478a4121c0ec61d4f938a57fa7a08b70586ee87
Larry Kagan <larry@superiocity.com>
2023-04-08 15:46:17 -0400

    Fix binary search in Python

--- miscellaneous/search/20230405/binary_search.py
+++ miscellaneous/search/20230405/binary_search.py
@@ -3,13 +3,13 @@ from typing import Tuple
 
 
 def binarySearch(nums: Tuple[int], target: int, start: int = None, end: int = None) -> int:
-    start = 0 if start is None else start = start
-    end = len(nums) - 1 if end is None else end = end
+    start = 0 if start is None else start
+    end = len(nums) - 1 if end is None else end
 
     if end - start < 0:
         return -1
 
-    mid = math.floor((start - end) / 2) + start
+    mid = math.floor((end - start) / 2) + start
 
     if target < nums[mid]:
         return binarySearch(nums, target, start, mid)
